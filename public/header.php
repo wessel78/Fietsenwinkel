89fcc569da31f00057824a0be4340a1d45c7a4e7
Wessel Bekken <63143367+wessel78@users.noreply.github.com>
2022-02-14 14:32:48 +0100

    Nav toegevoegd

--- public/header.php
+++ public/header.php
@@ -15,4 +15,31 @@
 </head>
 <body>
 
-    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Fietsenwinkel.nl</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <div class="form-inline my-2 my-lg-0">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="">Producten</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Winkelwagen</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inloggen</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Registeren</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
