335e2c7cabcb15777848ca3d1693b594ae26f3bc
mike doornenbal <mike@iconica.nl>
2022-02-15 11:06:21 +0100

    inlog en registreren

---
+++ public/registreren_pagina.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    require "header.php";
+    include "../src/classes/Database.php";
+    $db = new Database;
+?>
+
+<div class="page-wrapper">
+<form class="form-signin text-center">
+      <input type="email" id="inputEmail" class="form-control" placeholder="Username" required="" autofocus="">
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+    </form>
+</div>
+   
+
+<?php require "footer.php"; ?>
