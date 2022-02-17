b0776c553186f2b0733d19e80865f55015b9dd34
stagiair2 <victorpieper03@live.nl>
2022-02-17 10:27:16 +0100

    index en fiets

---
+++ public/fietsen.php
@@ -0,0 +1,155 @@
+<?php
+    session_start();
+    require "header.php";
+    include "../src/classes/Database.php";
+    $db = new Database;
+?>
+
+<div class="page-wrapper">
+    <div class="page-content-wrapper">
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 1</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 2</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 3</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 4</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 5</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 6</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 7</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 8</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 9</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="fietsen-card">
+            <div class="fietsen-card-header">
+                <img src="img/no-image-found.webp" alt="Geen afbeelding gevonden">
+            </div>
+            <div class="fietsen-card-title">
+                <p>Fiets 10</p>
+            </div>
+            <div class="fietsen-card-footer">
+                <div class="footer-wrapper">
+                    <p>$ 499,00</p>
+                    <a href="#">Add</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php require "footer.php";?>
--- public/index.php
+++ public/index.php
@@ -1,13 +1,25 @@
 <?php
-    session_start();
-    require "header.php";
-    include "../src/classes/Database.php";
-    $db = new Database;
+session_start();
+require "header.php";
+include "../src/classes/Database.php";
+$db = new Database;
 ?>
 
 <div class="page-wrapper">
-    
+    <div class="index-flexbox">
+        <div class="text-content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing.
+        </div>
+        <div class="photo-content">
+            <img src="../public/img/no-image-found.webp" class="companyphoto">
+            <p class="info-index">Plaats, land</p>
+            <p class="info-index">Adres </p>
+            <p class="info-index">postcode</p>
+            <p class="info-index">telefoon</p>
+            <p class="info-index">email </p>
+        </div>
+    </div>
 </div>
-   
+
 
 <?php require "footer.php"; ?>
