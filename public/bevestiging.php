b0e5ef25b74b0c5920ea2b04e4719c990b359c00
Victor Pieper <victorpieper03@live.nl>
2022-03-28 14:22:19 +0200

    EMAIL
    email functionaliteit

--- public/bevestiging.php
+++ public/bevestiging.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
+require "../src/functions/sendEmail.php";
 
 if ($_SESSION['login'] == "false") {
   header("Location: inlog_pagina.php");
   die();
 }
+
+sendEmail($_SESSION['email']);
 
 require "header.php";
 $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
